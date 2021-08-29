2cb7e9245fc84e5d07c227ec573fa20ee4d0bbe3
aaagit19 <chi.tenzyo@gmail.com>
2021-08-29 18:37:47 +0900

    0829

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,15 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import 'bootstrap';
+import '../stylesheets/application';
+import '../stylesheets/app';
+import '../stylesheets/products';
+import '../stylesheets/samazon';
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+var jQuery = require('jquery')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
